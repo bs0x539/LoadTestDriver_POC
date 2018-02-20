1eb0a9b6fb196e5d4dd34af313072270a54731fb
Christian Hilb <Christian.Hilb@icongmbh.de>
2018-02-20 13:50:27 +0100

    Format source code.

--- src/test/java/de/bs0x539/vaadin/TestFontIconButton.java
+++ src/test/java/de/bs0x539/vaadin/TestFontIconButton.java
@@ -1,34 +1,25 @@
 package de.bs0x539.vaadin;
 
-import com.vaadin.testbench.TestBenchTestCase;
-import com.vaadin.testbench.elements.ButtonElement;
-import com.vaadin.testbench.parallel.ParallelTest;
-import com.vaadin.testbench.parallel.ParallelTestSuite;
-import com.vaadin.ui.Button;
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.vaadin.johannest.loadtestdriver.LoadTestDriver;
 import org.vaadin.johannest.loadtestdriver.LoadTestDriverBuilder;
 
-import java.io.IOException;
+import com.vaadin.testbench.TestBenchTestCase;
+import com.vaadin.testbench.elements.ButtonElement;
 
 public class TestFontIconButton extends TestBenchTestCase {
 
-
   @Before
   public void init() {
-      WebDriver driver = new LoadTestDriverBuilder().
-          withIpAddress(LoadTestDriver.getLocalIpAddress()).
-          withNumberOfConcurrentUsers(1).
-          withRampUpTimeInSeconds(1).
-          withTestName("TestFontIconButton").
-          withPath("./gatling").
-          withResourcesPath("./gatling").
-          withStaticResourcesIngnoring().
-          withTestRefactoring().
-          build();
-      setDriver(driver);
+    final WebDriver driver = new LoadTestDriverBuilder().withIpAddress(LoadTestDriver.getLocalIpAddress())
+      .withNumberOfConcurrentUsers(1).withRampUpTimeInSeconds(1).withTestName("TestFontIconButton")
+      .withPath("./gatling").withResourcesPath("./gatling").withStaticResourcesIngnoring().withTestRefactoring()
+      .build();
+    setDriver(driver);
   }
 
   @Test
